63626af5b11142b7cbcdaef3acc2e805f1411de2
thabang <tsepeng@globaltrack.com>
2022-04-18 13:57:30 +0200

    created message consumer console application

---
+++ RabbitMqMessageConsumer/DisplayMessage/DisplayReceivedMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMqMessageConsumer.DisplayMessage
+{
+    public class DisplayReceivedMessage
+    {
+        public static string DisplayIncomingMessage(string message)
+        {
+            var pos = message.IndexOf(",");
+            var name = message.Substring(pos + 1).Trim('"');
+            var outputMessage = "";
+            if (!string.IsNullOrEmpty(name))
+            {
+                outputMessage = $"Hello {name}, I am your father!";
+                Console.WriteLine(outputMessage);
+            }
+            else
+            {
+                throw new ArgumentNullException("Please Ensure you have Entered your Name!");
+            }
+
+            return outputMessage;
+        }
+    }
+}
---
+++ RabbitMqMessageConsumer/MainMessageConsumer.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMqMessageConsumer.DisplayMessage;
+using System;
+using System.Text;
+
+namespace RabbitMqMessageConsumer
+{
+    public class MainMessageConsumer
+    {
+        public static void Main(string[] args)
+        {
+            ConsumeMessage();
+        }
+
+        private static void ConsumeMessage()
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("message-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                DisplayReceivedMessage.DisplayIncomingMessage(message);
+            };
+
+            channel.BasicConsume("message-queue", true, consumer);
+            Console.ReadLine();
+        }
+    }
+}
